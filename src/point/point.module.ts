d7cd32c4576f22cfa5131f0c548d7289f28eca71
be.lee <be.lee@univstore.com>
2024-06-20 01:02:19 +0900

    Feat: Add providers User in point.modules

--- src/point/point.module.ts
+++ src/point/point.module.ts
@@ -5,6 +5,10 @@ import { PointServicePort } from './controller/port/Point.service.port';
 import { PointServiceImpl } from './service/Point.service.impl';
 import { PointRepositoryPort } from './service/port/Point.repository.port';
 import { PointRepositoryImpl } from './infrastructure/Point.repository.impl';
+import { UserServicePort } from './controller/port/User.service.port';
+import { UserServiceImpl } from './service/User.service.impl';
+import { UserRepositoryPort } from './service/port/User.repository.port';
+import { UserRepositoryImpl } from './infrastructure/User.repository.impl';
 
 @Module({
   imports: [DatabaseModule],
@@ -12,6 +16,8 @@ import { PointRepositoryImpl } from './infrastructure/Point.repository.impl';
   providers: [
     { provide: PointServicePort, useClass: PointServiceImpl },
     { provide: PointRepositoryPort, useClass: PointRepositoryImpl },
+    { provide: UserServicePort, useClass: UserServiceImpl },
+    { provide: UserRepositoryPort, useClass: UserRepositoryImpl },
   ],
 })
 export class PointModule {}
